2649f79bf086b711bb7492676e8675d50290ff46
ThisisShoo <shuhan_zheng@qq.com>
2023-01-26 16:57:21 -0500

    Update csv to LaTeX Converter.py

--- csv LaTeX Converter/csv to LaTeX Converter.py
+++ csv LaTeX Converter/csv to LaTeX Converter.py
@@ -1,29 +1,30 @@
 """Written by Shuhan Zheng"""
 import csv
 
-def covert_csv(file_name):
+def convert_csv(file_name):
     """This function reads csv tables and converts them to LaTeX source codes that """
-    print (r'\being{tabular}[H]')
-    print (r'    \centering')
-    pring (r'    \caption{}')
-    table_begin = r'    \begin{table}[H]'
+    begin_table = r'\begin{table}[H]' + '\n' + '    \centering' + '\n' + '    \caption{}' +'\n' + '    \\begin{tabular}[H]'
     
     with open(f'{file_name}', mode = 'r', encoding='utf-8') as file:
         reader = csv.reader(file)
-        output = ''
-        for row in reader:
-            rowtext = ' & '.join(row)
-            rowtext = '        ' + rowtext + ' \\\\ ' + r'\\hline'
+        content = []
 
-            output += rowtext
-            print (rowtext)
-        print (r'\end{tabular}')
-    #return output
+        for row in reader:
+            content.append(' '*8 + ' & '.join(row))
+
+        col_def = "{" + "|l" * len(row) + "|}"
+
+    data = (r'\\ \hline' + '\n').join(content) + "\n"
+    begin_table += col_def + '\n'
+    end_table = "    \end{tabular}" + "\n" + r'\end{table}'
+
+    print (begin_table + data + end_table)
 
 filename = input('Enter the file name, including ".csv". Type "cancel" to cancel:')
+
 if filename == 'cancel':
     print ('Cancelled')
 else:
-    covert_csv(filename)
+    convert_csv(filename)
 
 print('You can now copy the latex source code above and add it to your document')
